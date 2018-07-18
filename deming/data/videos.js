583fb345dfedd8a4951bc0f26488c89af568d981
lepkon <lepkon@free.fr>
2018-07-18 13:11:32 +0200

    c deming videos

---
+++ deming/data/videos.js
@@ -0,0 +1,22 @@
+videos = [
+    {
+        "id": "GHvnIm9UEoQ",
+        "title": "Part 1",
+        "description": "The source of life's endless forms was a profound mystery until Charles Darwin's revolutionary idea of natural selection.\nNOVA reveals answers to the riddles that Darwin couldn't explain."
+    },
+    {
+        "id": "mKFGj8sK5R8",
+        "title": "Will the Earths Magnetic Fields Shift?",
+        "description": "Is the Earth losing its magnetic field and doomed to a fate similar to Mars? Many scientists believe the answer lies in paleomagnetic data, and that this weakening may be a precursor to a magnetic field reversal."
+    },
+    {
+        "id": "6WeTaLRb-Bs",
+        "title": "The Revelation Of The Pyramids",
+        "description": "The Revelation Of The Pyramids takes an indepth look into one of the seven wonders of the world, the Great Pyramids of Egypt. Mystery has surrounded these fascinating structures for centuries with theories varying from the scientific to the bizarre."
+    },    
+    {
+        "id": "vcG_Pmt_Ny4",
+        "title": "The City of London, the Corporation that runs it",
+        "description": "A secret state within a state, with deleterious effects on democracy, politics and economics in London, the country, and the world, for the City is joint headquarters with Wall Street of global finance capital. \nIn short, 'Secret City' isn't just a film for Londoners - especially in these times of crisis, the role of the City concerns everyone everywhere."
+    }
+]
